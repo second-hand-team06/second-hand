0f174d467b4b5dddaaf9f4877dc1d40fabe5be5f
YouSooA <kinsk2839@naver.com>
2023-06-06 22:32:13 +0900

    refactor: Route 컴포넌트 주석 제거
    - Home, NeighborhoodSetting 컴포넌트 import

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from '@pages/Home';
+import NeighborhoodSetting from '@pages/NeighborhoodSetting';
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route index path="/" element={<Home />} />
-        <Route path="/neighborhood-setting" element={<NeighborhoodSetting />} /> */}
+        <Route index path="/" element={<Home />} />
+        <Route path="/neighborhood-setting" element={<NeighborhoodSetting />} />
       </Routes>
     </BrowserRouter>
   );

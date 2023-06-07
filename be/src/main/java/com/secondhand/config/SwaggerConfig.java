38d9d4e762bdf3be638f9b1266ea186a238474b4
birdieHyun <flaehdan@gmail.com>
2023-06-07 16:03:38 +0900

    feat: swagger 설정 변경(#11)

--- be/src/main/java/com/secondhand/config/SwaggerConfig.java
+++ be/src/main/java/com/secondhand/config/SwaggerConfig.java
@@ -22,7 +22,9 @@ public class SwaggerConfig {
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("Second Hand API")
-                        .description("Second Hand API 명세서입니다.")
+                        .description("Second Hand API 명세서입니다." +
+                                " \n 시간은 yyyy-MM-dd kk:mm:ss 형식으로 표시됩니다." +
+                                " \n swagger 오류 때문에 시간이 2023-06-07T07:00:52.146Z 형식으로 표시됩니다.")
                         .version("v0.0.1"));
     }
 }

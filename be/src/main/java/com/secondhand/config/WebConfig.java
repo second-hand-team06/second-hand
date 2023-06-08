5d2e58a7c0b0ef5bf376fc912adc7a1358903dc9
birdieHyun <flaehdan@gmail.com>
2023-06-08 20:44:21 +0900

    feat: FE와 통신하기 위한 CORS 설정
    CORS 학습 후 설정 변경 예정

---
+++ be/src/main/java/com/secondhand/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.secondhand.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// TODO : CORS 학습 후 addMapping, allowedOrigin, allowedMethods 설정
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}

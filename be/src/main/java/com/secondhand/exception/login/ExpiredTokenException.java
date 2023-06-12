0167002178729adbde95273d342773da567ec6c5
acceptor-gyu <gseonggyu968@gmail.com>
2023-06-13 07:25:03 +0900

    feat: 로그인 시 커스텀 에러 클래스 생성

---
+++ be/src/main/java/com/secondhand/exception/login/ExpiredTokenException.java
@@ -0,0 +1,8 @@
+package com.secondhand.exception.login;
+
+public class ExpiredTokenException extends JwtTokenException{
+
+    public ExpiredTokenException() {
+        super("Token is expired");
+    }
+}

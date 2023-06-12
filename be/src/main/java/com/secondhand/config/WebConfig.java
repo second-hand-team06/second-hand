01f95e9df08b221eedf498bf0d0e4aabc1d6d416
acceptor-gyu <gseonggyu968@gmail.com>
2023-06-13 07:19:35 +0900

    feat: JwtUtil 객체를 스프링이 주입하도록 설정

--- be/src/main/java/com/secondhand/config/WebConfig.java
+++ be/src/main/java/com/secondhand/config/WebConfig.java
@@ -1,12 +1,20 @@
 package com.secondhand.config;
 
+import com.secondhand.interceptor.LoginInterceptor;
+import com.secondhand.user.login.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // TODO : CORS 학습 후 addMapping, allowedOrigin, allowedMethods 설정
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtUtil jwtUtil;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -14,10 +22,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*");
     }
 
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new LoginInterceptor(new JwtUtil()))
-//                .addPathPatterns("/posts/**")
-//                .excludePathPatterns("/posts");
-//    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor(jwtUtil))
+                .addPathPatterns("/posts/**")
+                .excludePathPatterns("/posts");
+    }
 }

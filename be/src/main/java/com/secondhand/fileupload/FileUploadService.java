8d6393d384c731f56d159183d94b95c993d33cd7
acceptor-gyu <gseonggyu968@gmail.com>
2023-06-12 19:03:13 +0900

    feat: 이미지 파일 업로드 메서드 구현(#28)

--- be/src/main/java/com/secondhand/fileupload/FileUploadService.java
+++ be/src/main/java/com/secondhand/fileupload/FileUploadService.java
@@ -8,9 +8,9 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,21 +22,26 @@ public class FileUploadService {
     private String bucketName;
 
 
-    public PutObjectResponse uploadFile(MultipartFile file) {
-        String fileName = file.getOriginalFilename();
+    public String uploadFile(MultipartFile file) {
+
+        String randomId = UUID.randomUUID().toString().substring(0, 10);
+        String fileName = file.getOriginalFilename() + randomId;
         String contentType = file.getContentType();
 
         // Upload file
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
-                .key("profile/" + fileName)
+                .key("img/" + fileName)
                 .acl("public-read")
                 .contentDisposition("inline")
                 .contentType(contentType)
                 .build();
 
+        String url = "https://" + bucketName + ".s3.ap-northeast-2.amazonaws.com/img/" + fileName;
+
         try {
-            return s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+            return url;
         } catch (IOException e) {
             throw new IllegalStateException("Failed to upload file", e);
         }

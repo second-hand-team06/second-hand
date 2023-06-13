9763516a25dd0b573a1286171d94506d33d00066
acceptor-gyu <gseonggyu968@gmail.com>
2023-06-13 16:38:23 +0900

    fix: 판매글 등록 후 상세 페이지로 이동하기 위해 postId를 리턴하도록 함(#28)

--- be/src/main/java/com/secondhand/post/dto/PostSaveDto.java
+++ be/src/main/java/com/secondhand/post/dto/PostSaveDto.java
@@ -10,7 +10,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
-
 @Getter @Setter @AllArgsConstructor
 public class PostSaveDto {
 

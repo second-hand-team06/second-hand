571dd4d9170b970d64c080d621bb3780858e9ae1
YouSooA <kinsk2839@naver.com>
2023-06-14 17:42:29 +0900

    feat: ProductList에서 무한 스크롤 기능 구현 (#17)
    - target 요소에 닿으면 페이지 증가 (useIntersectionObserver)

--- fe/src/components/ProductList/index.tsx
+++ fe/src/components/ProductList/index.tsx
@@ -1,28 +1,61 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import useFetch from '@hooks/useFetch';
+import useIntersectionObserver from '@hooks/useIntersectionObserver';
 
 import ProductListItem, { ProductListItemProps } from '@components/ProductListItem';
 import * as S from './style';
 
 interface PostsApiResponse {
-  posts: { content: ProductListItemProps[] };
+  posts: { content: ProductListItemProps[]; last: boolean };
 }
 
 const ProductList = () => {
   const [pageNum, setPageNum] = useState(0);
+  const [postList, setPostList] = useState<ProductListItemProps[]>([]);
+  const scrollPositionRef = useRef(0);
 
-  const { fetchState } = useFetch<PostsApiResponse>({
+  const { fetchData, fetchState } = useFetch<PostsApiResponse>({
     url: `http://13.124.150.120:8080/posts?page=${pageNum}&size=10&category=1`,
     method: 'GET',
   });
 
+  const intersectHandler: IntersectionObserverCallback = ([entry]) => {
+    if (!entry.isIntersecting) return;
+
+    if (fetchState.state === 'SUCCESS') {
+      setPageNum(pageNum + 1);
+    }
+  };
+
+  const { setTarget } = useIntersectionObserver({ intersectHandler });
+
+  useEffect(() => {
+    fetchData();
+    scrollPositionRef.current = window.scrollY;
+  }, [pageNum]);
+
+  useEffect(() => {
+    if (fetchState.state !== 'SUCCESS' || !fetchState.data) return;
+
+    const { posts } = fetchState.data;
+    setPostList((previous) => [...previous, ...posts.content]);
+
+    window.scrollTo(0, scrollPositionRef.current);
+  }, [fetchState.state, fetchState.data]);
+
   return (
     <S.ProductList>
       {fetchState.state === 'LOADING' && <h1>Loading</h1>}
-      {fetchState.state === 'ERROR' && fetchState.error && <h1>Error</h1>}
-      {fetchState.data &&
-        fetchState.data.posts.content.map((item) => <ProductListItem key={item.id} {...item} />)}
+      {fetchState.state === 'ERROR' && <h1>Error</h1>}
+      {fetchState.state === 'SUCCESS' && fetchState.data && (
+        <>
+          {postList.map((item) => (
+            <ProductListItem key={item.id} {...item} />
+          ))}
+          {!fetchState.data.posts.last && <div ref={setTarget}></div>}
+        </>
+      )}
     </S.ProductList>
   );
 };

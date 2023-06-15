660700c96c0f1ec4262b384dd5c0bd5a5f65db63
YouSooA <kinsk2839@naver.com>
2023-06-15 23:19:09 +0900

    refactor: Home 페이지의 Header를 컴포넌트로 분리 (#52)
    - 전체 렌더링을 방지를 위해 Header만 분리
    - 추후에 Header 컴포넌트 수정 예정

--- fe/src/pages/Home/index.tsx
+++ fe/src/pages/Home/index.tsx
@@ -1,27 +1,14 @@
-import { Link } from 'react-router-dom';
-
-import { ICON_NAME } from '@constants/index';
-
-import Icon from '@components/common/Icon';
-import TabBar from '@components/TabBar';
+import Header from '@components/common/Header';
 import ProductList from '@components/ProductList';
-import * as S from './style';
+import TabBar from '@components/TabBar';
 
 const Home = () => {
   return (
-    <div>
-      <S.NavigationBar>
-        <S.NeighborhoodSettingLink>
-          <span>역삼 1동</span>
-          <Icon name={ICON_NAME.CHEVRON_DOWN} />
-        </S.NeighborhoodSettingLink>
-        <Link to="/category">
-          <Icon name={ICON_NAME.HAMBURGER} />
-        </Link>
-      </S.NavigationBar>
+    <>
+      <Header />
       <ProductList />
       <TabBar activeTab="home" />
-    </div>
+    </>
   );
 };
 

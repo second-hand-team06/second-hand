d0a800f213e8abb5045535dfb4a7006d7c0a643c
YouSooA <kinsk2839@naver.com>
2023-06-15 18:32:15 +0900

    feat: Category 페이지 생성 (#52)
    - 아무런 기능 없는 초기 Category 페이지 생성

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@context/themeContext';
 
 import Home from '@pages/Home';
 import NeighborhoodSetting from '@pages/NeighborhoodSetting';
+import Category from '@pages/Category';
 import Sales from '@pages/Sales';
 import Interests from '@pages/Interests';
 import Chatting from '@pages/Chatting';
@@ -20,6 +21,7 @@ const App = () => {
         <Routes>
           <Route index path="/" element={<Home />} />
           <Route path="/neighborhood-setting" element={<NeighborhoodSetting />} />
+          <Route path="/category" element={<Category />} />
           <Route path="/sales" element={<Sales />} />
           <Route path="/interests" element={<Interests />} />
           <Route path="/chatting" element={<Chatting />} />

5e27b0373c63ce0dd252651edca6ffc6a80d3a5a
YouSooA <kinsk2839@naver.com>
2023-06-15 17:26:06 +0900

    feat: Home 페이지의 NavigationBar 생성 (#52)
    - styled-components로 생성

--- fe/src/pages/Home/index.tsx
+++ fe/src/pages/Home/index.tsx
@@ -1,11 +1,21 @@
 import { ICON_NAME } from '@constants/index';
 
+import Icon from '@components/common/Icon';
 import TabBar from '@components/TabBar';
+import ProductList from '@components/ProductList';
+import * as S from './style';
 
 const Home = () => {
   return (
     <div>
-      <span>Home</span>
+      <S.NavigationBar>
+        <S.NeighborhoodSettingLink>
+          <span>역삼 1동</span>
+          <Icon name={ICON_NAME.CHEVRON_DOWN} />
+        </S.NeighborhoodSettingLink>
+        <Icon name={ICON_NAME.HAMBURGER} />
+      </S.NavigationBar>
+      <ProductList />
       <TabBar activeTab="home" />
     </div>
   );
--- fe/src/pages/Home/style.ts
+++ fe/src/pages/Home/style.ts
@@ -1,1 +1,28 @@
-export {};
+import styled from 'styled-components';
+
+const NavigationBar = styled.header`
+  position: sticky;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+
+  width: 100%;
+  height: 44px;
+  padding: 11px 16px;
+
+  background-color: ${({ theme }) => theme.colors.neutral.background.blur};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.neutral.border.default};
+`;
+
+const NeighborhoodSettingLink = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+
+  color: ${({ theme }) => theme.colors.neutral.text.strong};
+  font-size: ${({ theme }) => theme.fonts.headline.fontSize};
+  font-weight: ${({ theme }) => theme.fonts.headline.fontWeight};
+  line-height: ${({ theme }) => theme.fonts.headline.lineHeight};
+`;
+
+export { NavigationBar, NeighborhoodSettingLink };

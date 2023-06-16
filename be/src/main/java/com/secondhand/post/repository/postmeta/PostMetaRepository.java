ee109c9b85f2562b97cf8d495c6d0bd6ad13eb7a
birdieHyun <flaehdan@gmail.com>
2023-06-16 21:26:44 +0900

    refatcor: 사용하지 않는 코드 삭제 (#84)

--- be/src/main/java/com/secondhand/post/repository/postmeta/PostMetaRepository.java
+++ be/src/main/java/com/secondhand/post/repository/postmeta/PostMetaRepository.java
@@ -4,6 +4,4 @@ import com.secondhand.post.entity.PostMeta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostMetaRepository extends JpaRepository<PostMeta, Long>, PostMetaRepositoryCustom {
-
-//    List<Interest> findPostsWithIntere
 }

260f0c7b48ac34d2cf1de57b2b9b30784b03dce8
saseung <seung0122@naver.com>
2023-06-16 13:58:47 +0900

    refactor: 백그라운드 컬러 theme에서 palette 변수로 변경
    - 기획서에 정의되지않은 색상일 경우 palette에서 색상 지정하는걸로 컨벤션 변경

--- fe/src/pages/MyAccount/style.ts
+++ fe/src/pages/MyAccount/style.ts
@@ -1,20 +1,22 @@
 import styled from 'styled-components';
 
+import { PALETTE } from '@styles/color';
+
 const MyAccount = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  
+  height: 100vh;
 `;
 
 const LoginButton = styled.button`
-  position: absolute;
-  bottom: 500px;
-  
   width: 80%;
   height: 52px;
 
   border-radius: 16px;
-  background-color: ${({ theme }) =>
-    theme.colors.accent.text.weak}; // 로그인 버튼에 대한 theme지정해줘야할지 고민
+  background-color: ${PALETTE.BLACK};
 
   font-size: 15px;
   font-weight: 600;

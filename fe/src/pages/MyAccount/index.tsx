681185cb17409e941178b92ea1930d8e0da21083
saseung <seung0122@naver.com>
2023-06-16 11:35:01 +0900

    feat: 로그인 화면 구현
    - 깃헙으로 로그인 버튼 누르면 깃헙 인증 페이지로 이동
    - 로그인 컴포넌트 구현

--- fe/src/pages/MyAccount/index.tsx
+++ fe/src/pages/MyAccount/index.tsx
@@ -1,12 +1,28 @@
 import TabBar from '@components/TabBar';
 
+import NavBar from '@components/NavBar';
+import * as S from './style';
+
 const MyAccount = () => {
+  const CLIENT_ID = '50e7f2067dc2cf55aa36';
+  const GITHUB_OAUTH_URL =
+    `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user%20public_repo`
+
+  const loginRequestHandler = () => {
+    return window.location.assign(GITHUB_OAUTH_URL);
+  };
+
   return (
-    <div>
-      <span>MyAccount</span>
+    <S.MyAccount>
+      <NavBar></NavBar>
+      <S.LoginButton onClick={loginRequestHandler}>GitHub 계정으로 로그인</S.LoginButton>
       <TabBar activeTab="myAccount" />
-    </div>
+      <button>11</button>
+    </S.MyAccount>
   );
 };
 
 export default MyAccount;
+
+
+

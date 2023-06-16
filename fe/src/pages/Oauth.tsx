be1568dbb68eeafebfb6696ac6ad036dd2a99442
saseung <seung0122@naver.com>
2023-06-16 13:57:01 +0900

    style: getToken 위치 변경
    - getToken 함수 선언을 useEffect  바깥에 분리

--- fe/src/pages/Oauth.tsx
+++ fe/src/pages/Oauth.tsx
@@ -7,26 +7,27 @@ const Oauth = () => {
   const urlParams = new URLSearchParams(queryString);
   const codeParam = urlParams.get('code');
 
+  const getToken = async () => {
+    try {
+      // todo: useFetch 사용예정
+      const response = await fetch(`http://13.124.150.120:8080/oauth?code=${codeParam}`);
+      const { token } = await response.json();
+
+      if (token && !localStorage.getItem('Token')) {
+        localStorage.setItem('Token', token);
+      }
+
+      navigate('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     if (!codeParam) return;
-    const getToken = async () => {
-      try {
-        // todo: useFetch 사용예정
-        console.log(codeParam);
-        const response = await fetch(`http://13.124.150.120:8080/oauth?code=${codeParam}`);
-        const { token } = await response.json();
-
-        if (token && !localStorage.getItem('Token')) {
-          localStorage.setItem('Token', token);
-        }
-        navigate('/');
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
+    
     getToken();
-  }, [location]);
+  }, []);
 
   return <div>loading</div>;
 };

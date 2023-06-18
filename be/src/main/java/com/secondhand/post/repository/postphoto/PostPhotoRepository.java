0c4e876fe4f32fb858429495204934fee7982b74
birdieHyun <flaehdan@gmail.com>
2023-06-18 14:43:32 +0900

    refactor: 데이터를 변경하는 작업이기에 Modifying 어노테이션 추가

--- be/src/main/java/com/secondhand/post/repository/postphoto/PostPhotoRepository.java
+++ be/src/main/java/com/secondhand/post/repository/postphoto/PostPhotoRepository.java
@@ -2,8 +2,10 @@ package com.secondhand.post.repository.postphoto;
 
 import com.secondhand.post.entity.PostPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 public interface PostPhotoRepository extends JpaRepository<PostPhoto, Long>, PostPhotoRepositoryCustom {
 
+    @Modifying
     void deleteAllByPostMetaId(Long postMetaId);
 }

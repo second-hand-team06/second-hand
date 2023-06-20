9286a9eafd7334e40e95daa2448900f9a1efc383
YouSooA <kinsk2839@naver.com>
2023-06-21 00:26:28 +0900

    refactor:  useFetch 변경에 따라 LoginLoading 변경 (#101)
    - useFetch의 반환값 변경으로 인해 리팩토링

--- fe/src/pages/LoginLoading/index.tsx
+++ fe/src/pages/LoginLoading/index.tsx
@@ -1,7 +1,12 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { REQUEST_METHOD } from '@constants/index';
+
 import useFetch from '@hooks/useFetch';
+
+// TODO: BE와 데이터 구조 협의
+type Data = string;
 
 const LoginLoading = () => {
   const navigate = useNavigate();
@@ -9,19 +14,17 @@ const LoginLoading = () => {
   const urlParams = new URLSearchParams(queryString);
   const codeParam = urlParams.get('code');
 
-  const { fetchState } = useFetch<string>({
+  const { data: token } = useFetch<Data>({
     url: `http://13.124.150.120:8080/oauth?code=${codeParam}`,
-    method: 'GET',
+    method: REQUEST_METHOD.GET,
   });
 
   useEffect(() => {
-    const { data: token } = fetchState;
-
     if (token && !localStorage.getItem('Token')) {
       localStorage.setItem('Token', token);
       navigate('/');
     }
-  }, [fetchState.data]);
+  }, [token]);
 
   return <div>loading</div>;
 };

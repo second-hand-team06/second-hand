f0b3e8422b16c9467acf1badeb5ab6a5f9c2122a
saseung <seung0122@naver.com>
2023-06-20 18:57:58 +0900

    feat: useFetch 활용해서 로직 수정 (#93)
    - useFetch 커스텀 훅 사용해서 로직 수정 및 타입 지정

--- fe/src/pages/LoginLoading/index.tsx
+++ fe/src/pages/LoginLoading/index.tsx
@@ -1,33 +1,27 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import useFetch from '@hooks/useFetch';
+
 const LoginLoading = () => {
   const navigate = useNavigate();
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const codeParam = urlParams.get('code');
 
-  const getToken = async () => {
-    try {
-      // todo: useFetch 사용예정
-      const response = await fetch(`http://13.124.150.120:8080/oauth?code=${codeParam}`);
-      const { token } = await response.json();
-
-      if (token && !localStorage.getItem('Token')) {
-        localStorage.setItem('Token', token);
-      }
-
-      navigate('/');
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const { fetchState } = useFetch<string>({
+    url: `http://13.124.150.120:8080/oauth?code=${codeParam}`,
+    method: 'GET',
+  });
 
   useEffect(() => {
-    if (!codeParam) return;
+    const { data: token } = fetchState;
 
-    getToken();
-  }, []);
+    if (token && !localStorage.getItem('Token')) {
+      localStorage.setItem('Token', token);
+      navigate('/');
+    }
+  }, [fetchState.data]);
 
   return <div>loading</div>;
 };

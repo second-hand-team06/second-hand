4b24194fa3ba8b0327ad2d32831b1be7aafc10bf
YouSooA <kinsk2839@naver.com>
2023-06-21 18:04:27 +0900

    feat: [< 뒤로] 버튼을 클릭하면 Home 페이지로 이동하는 기능 (#112)

--- fe/src/pages/Category/index.tsx
+++ fe/src/pages/Category/index.tsx
@@ -2,14 +2,17 @@ import { ICON_NAME } from '@constants/index';
 
 import Icon from '@components/common/Icon';
 import * as S from './style';
+import { Link } from 'react-router-dom';
 
 const Category = () => {
   return (
     <S.Header>
-      <S.BackButton>
-        <Icon name={ICON_NAME.CHEVRON_LEFT} />
-        <span>뒤로</span>
-      </S.BackButton>
+      <Link to="/">
+        <S.BackButton>
+          <Icon name={ICON_NAME.CHEVRON_LEFT} />
+          <span>뒤로</span>
+        </S.BackButton>
+      </Link>
       <S.Title>카테고리</S.Title>
       <S.EmptyTag></S.EmptyTag>
     </S.Header>

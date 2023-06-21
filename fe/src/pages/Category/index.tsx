0d75d18771a9615b6a046c626e99bbed2741f3d5
YouSooA <kinsk2839@naver.com>
2023-06-21 18:02:07 +0900

    feat: Category 페이지 Header View 생성 (#112)

--- fe/src/pages/Category/index.tsx
+++ fe/src/pages/Category/index.tsx
@@ -1,5 +1,19 @@
+import { ICON_NAME } from '@constants/index';
+
+import Icon from '@components/common/Icon';
+import * as S from './style';
+
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <S.Header>
+      <S.BackButton>
+        <Icon name={ICON_NAME.CHEVRON_LEFT} />
+        <span>뒤로</span>
+      </S.BackButton>
+      <S.Title>카테고리</S.Title>
+      <S.EmptyTag></S.EmptyTag>
+    </S.Header>
+  );
 };
 
 export default Category;
---
+++ fe/src/pages/Category/style.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 45px;
+
+  height: 44px;
+  padding: 0 9px;
+
+  border-bottom: 1px solid ${({ theme }) => theme.colors.neutral.border.default};
+`;
+
+const Title = styled.span`
+  font-size: ${({ theme }) => theme.fonts.body.bold.fontSize};
+  font-weight: ${({ theme }) => theme.fonts.body.bold.fontWeight};
+  line-height: ${({ theme }) => theme.fonts.body.bold.lineHeight};
+  color: ${({ theme }) => theme.colors.neutral.text.strong};
+`;
+
+const BackButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  font-size: ${({ theme }) => theme.fonts.body.regular.fontSize};
+  font-weight: ${({ theme }) => theme.fonts.body.regular.fontWeight};
+  line-height: ${({ theme }) => theme.fonts.body.regular.lineHeight};
+  color: ${({ theme }) => theme.colors.neutral.text.strong};
+`;
+
+const EmptyTag = styled.div``;
+
+export { Header, BackButton, Title, EmptyTag };

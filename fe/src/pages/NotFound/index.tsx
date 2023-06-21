ab5cc4151620fd268ca264f939757de0135a253a
YouSooA <kinsk2839@naver.com>
2023-06-21 14:10:21 +0900

    feat: Home 페이지 버튼, 이전 페이지 버튼 라우터 연결 (#103)

--- fe/src/pages/NotFound/index.tsx
+++ fe/src/pages/NotFound/index.tsx
@@ -1,11 +1,17 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 import * as S from './style';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <S.NotFound>
       <S.ErrorMessage>404 Page Not Found</S.ErrorMessage>
-      <S.PageButton>Home 페이지로 이동</S.PageButton>
-      <S.PageButton>이전 페이지로 이동</S.PageButton>
+      <Link to="/">
+        <S.PageButton>Home 페이지로 이동</S.PageButton>
+      </Link>
+      <S.PageButton onClick={() => navigate(-1)}>이전 페이지로 이동</S.PageButton>
     </S.NotFound>
   );
 };

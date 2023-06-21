9bcba46eabfe451e6039c18cb0dc19da730cde2a
YouSooA <kinsk2839@naver.com>
2023-06-22 00:17:02 +0900

    feat: category 데이터 응답 상태에 따라 View 분리 (#120)
    - responseState에 따라 화면 View 분리
    - LOADING, ERROR, SUCCESS일 경우 각자 화면에 보여줄 View 생성

--- fe/src/pages/Category/index.tsx
+++ fe/src/pages/Category/index.tsx
@@ -1,21 +1,46 @@
-import { ICON_NAME } from '@constants/index';
+import { Link } from 'react-router-dom';
+
+import { ICON_NAME, REQUEST_METHOD, RESPONSE_STATE } from '@constants/index';
+
+import useFetch from '@hooks/useFetch';
 
 import Icon from '@components/common/Icon';
 import * as S from './style';
-import { Link } from 'react-router-dom';
+
+interface Category {
+  id: number;
+  name: string;
+  photoUrl: string;
+}
+
+interface CategoriesData {
+  categories: Category[];
+}
 
 const Category = () => {
+  const { responseState, data } = useFetch<CategoriesData>({
+    url: 'http://13.124.150.120:8080/categories',
+    method: REQUEST_METHOD.GET,
+  });
+
   return (
-    <S.Header>
-      <Link to="/">
-        <S.BackButton>
-          <Icon name={ICON_NAME.CHEVRON_LEFT} />
-          <span>뒤로</span>
-        </S.BackButton>
-      </Link>
-      <S.HeaderTitle>카테고리</S.HeaderTitle>
-      <S.EmptyTag></S.EmptyTag>
-    </S.Header>
+    <>
+      <S.Header>
+        <Link to="/">
+          <S.BackButton>
+            <Icon name={ICON_NAME.CHEVRON_LEFT} />
+            <span>뒤로</span>
+          </S.BackButton>
+        </Link>
+        <S.HeaderTitle>카테고리</S.HeaderTitle>
+        <S.EmptyTag></S.EmptyTag>
+      </S.Header>
+
+      {responseState === RESPONSE_STATE.LOADING && <div>loading</div>}
+      {responseState === RESPONSE_STATE.ERROR && <div>error</div>}
+      {responseState === RESPONSE_STATE.SUCCESS &&
+        data?.categories.map((item) => <div key={item.id}>{item.name}</div>)}
+    </>
   );
 };
 

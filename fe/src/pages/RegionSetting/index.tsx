69e5444b6f3e935fb204eb52d455a332219e10ef
YouSooA <kinsk2839@naver.com>
2023-06-22 21:34:21 +0900

    feat: 사용자가 등록한 동네들을 버튼으로 화면에 표시 (#132)
    - 사용자가 등록한 동네에 대한 데이터 GET 요청

--- fe/src/pages/RegionSetting/index.tsx
+++ fe/src/pages/RegionSetting/index.tsx
@@ -1,12 +1,43 @@
+import { ICON_NAME, REQUEST_METHOD } from '@constants/index';
+
+import useFetch from '@hooks/useFetch';
+
+import Icon from '@components/common/Icon';
+import Button from '@components/common/Button';
 import * as S from './style';
 
+interface Region {
+  id: number;
+  name: string;
+}
+
+interface RegionsData {
+  regions: Region[];
+}
+
 const RegionSetting = () => {
+  const { data } = useFetch<RegionsData>({
+    url: 'http://13.124.150.120:8080/users/regions',
+    method: REQUEST_METHOD.GET,
+  });
+
   return (
-    <S.Header>
-      <S.CloseButton>닫기</S.CloseButton>
-      <S.HeaderTitle>동네 설정</S.HeaderTitle>
-      <S.EmptyTag />
-    </S.Header>
+    <>
+      <S.Header>
+        <S.CloseButton>닫기</S.CloseButton>
+        <S.HeaderTitle>동네 설정</S.HeaderTitle>
+        <S.EmptyTag />
+      </S.Header>
+      {data?.regions.map(({ id, name }, idx) => (
+        <Button key={id} buttonType="rectangle" buttonState={idx === 0 ? 'active' : 'default'}>
+          <span>{name}</span>
+          <Icon name={ICON_NAME.MULTIPLY} />
+        </Button>
+      ))}
+      <Button buttonType="rectangle" buttonState="default">
+        <Icon name={ICON_NAME.PLUS} />
+      </Button>
+    </>
   );
 };
 

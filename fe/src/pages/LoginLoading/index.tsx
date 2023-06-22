80322efabd8b4726095807669e485624bf70b12d
saseung <seung0122@naver.com>
2023-06-22 17:55:39 +0900

    refactor: 데이터 형식 변환으로 loginLoading 로직 수정 (#134)

--- fe/src/pages/LoginLoading/index.tsx
+++ fe/src/pages/LoginLoading/index.tsx
@@ -5,8 +5,9 @@ import { REQUEST_METHOD, RESPONSE_STATE } from '@constants/index';
 
 import useFetch from '@hooks/useFetch';
 
-// TODO: BE와 데이터 구조 협의
-type Data = string;
+interface TokenData {
+  token: string;
+}
 
 const LoginLoading = () => {
   const navigate = useNavigate();
@@ -14,7 +15,7 @@ const LoginLoading = () => {
   const urlParams = new URLSearchParams(queryString);
   const codeParam = urlParams.get('code');
 
-  const { responseState, data: token } = useFetch<Data>({
+  const { responseState, data } = useFetch<TokenData>({
     url: `http://13.124.150.120:8080/oauth?code=${codeParam}`,
     method: REQUEST_METHOD.GET,
   });
@@ -24,14 +25,16 @@ const LoginLoading = () => {
 
     if (isLoggedIn) navigate('/');
 
-    if (responseState !== RESPONSE_STATE.SUCCESS) return;
+    if (responseState !== RESPONSE_STATE.SUCCESS || !data) return;
+
+    const { token } = data;
 
     if (token) {
       localStorage.setItem('Token', token);
     }
 
     navigate('/');
-  }, [responseState, token]);
+  }, [responseState, data]);
 
   return <div>loading</div>;
 };

d8c5a26c4825cb1ee35363b0da7cc945e72ffb39
YouSooA <kinsk2839@naver.com>
2023-06-22 16:16:41 +0900

    feat: Home의 Header에 사용자가 지정한 region 표시 (#100)

--- fe/src/components/common/Header/HomeHeader/index.tsx
+++ fe/src/components/common/Header/HomeHeader/index.tsx
@@ -1,18 +1,43 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
-import { ICON_NAME } from '@constants/index';
+import { ICON_NAME, REQUEST_METHOD } from '@constants/index';
+
+import useFetch from '@hooks/useFetch';
 
 import Icon from '@components/common/Icon';
 import * as S from './style';
 
+interface Region {
+  id: number;
+  name: string;
+}
+
+interface RegionsData {
+  regions: Region[];
+}
+
 const HomeHeader = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data } = useFetch<RegionsData>({
+    url: 'http://13.124.150.120:8080/users/regions',
+    method: REQUEST_METHOD.GET,
+  });
+  const isLoggedIn = localStorage.getItem('Token');
+
+  const firstDefaultRegion = '역삼 1동';
+  const defaultRegion = useMemo(() => {
+    if (!data) return;
+
+    const address = data.regions[0].name;
+
+    return address.split(' ').at(-1);
+  }, [data]);
 
   return (
     <S.HomeHeader>
       <S.NeighborhoodDropdown onClick={() => setIsModalOpen(!isModalOpen)}>
-        <span>역삼 1동</span>
+        <span>{isLoggedIn ? defaultRegion : firstDefaultRegion}</span>
         <Icon name={ICON_NAME.CHEVRON_DOWN} />
         {isModalOpen && (
           <S.Modal>

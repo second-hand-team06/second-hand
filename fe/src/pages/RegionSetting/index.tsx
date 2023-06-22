41d98730b1451eecd7b000a49e15390310ec1090
YouSooA <kinsk2839@naver.com>
2023-06-22 18:20:02 +0900

    feat: 동네 설정 페이지의 Header View 생성(#132)

--- fe/src/pages/RegionSetting/index.tsx
+++ fe/src/pages/RegionSetting/index.tsx
@@ -1,5 +1,13 @@
+import * as S from './style';
+
 const RegionSetting = () => {
-  return <div>RegionSetting</div>;
+  return (
+    <S.Header>
+      <S.CloseButton>닫기</S.CloseButton>
+      <S.HeaderTitle>동네 설정</S.HeaderTitle>
+      <S.EmptyTag />
+    </S.Header>
+  );
 };
 
 export default RegionSetting;
--- fe/src/pages/RegionSetting/style.ts
+++ fe/src/pages/RegionSetting/style.ts
@@ -1,1 +1,38 @@
-export {};
+import styled from 'styled-components';
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 44px;
+  padding: 0 9px;
+
+  border-bottom: 1px solid ${({ theme }) => theme.colors.neutral.border.default};
+`;
+
+const HeaderTitle = styled.span`
+  font-size: ${({ theme }) => theme.fonts.body.bold.fontSize};
+  font-weight: ${({ theme }) => theme.fonts.body.bold.fontWeight};
+  line-height: ${({ theme }) => theme.fonts.body.bold.lineHeight};
+  color: ${({ theme }) => theme.colors.neutral.text.strong};
+`;
+
+const CloseButton = styled.button`
+  display: flex;
+  align-items: center;
+
+  width: 105px;
+  height: 42px;
+
+  font-size: ${({ theme }) => theme.fonts.body.regular.fontSize};
+  font-weight: ${({ theme }) => theme.fonts.body.regular.fontWeight};
+  line-height: ${({ theme }) => theme.fonts.body.regular.lineHeight};
+  color: ${({ theme }) => theme.colors.neutral.text.default};
+`;
+
+const EmptyTag = styled.div`
+  width: 105px;
+`;
+
+export { Header, HeaderTitle, CloseButton, EmptyTag };

0efc396d28798237f57648815132ce7a3b88ad66
YouSooA <kinsk2839@naver.com>
2023-06-23 13:38:21 +0900

    feat: PATH 상수 생성 (#104)

---
+++ fe/src/constants/path.ts
@@ -0,0 +1,15 @@
+const PATH = {
+  HOME: '/',
+  REGION_SETTING: '/region-setting',
+  CATEGORY: '/categories',
+  CATEGORY_DETAIL: '/categories/detail',
+  SALES: '/sales',
+  INTERESTS: '/interests',
+  CHATTING: '/chatting',
+  MY_ACCOUNT: '/my-account',
+  LOGIN_LOADING: '/login-loading',
+  NEW_PRODUCT: '/new-product',
+  PRODUCT_DETAIL: '/product-detail/:id',
+} as const;
+
+export { PATH };

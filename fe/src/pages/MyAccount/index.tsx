41c3934335514eeffa52d4db96cdae071bddf92c
YouSooA <kinsk2839@naver.com>
2023-06-23 14:50:01 +0900

    feat: env 적용 (#104)

--- fe/src/pages/MyAccount/index.tsx
+++ fe/src/pages/MyAccount/index.tsx
@@ -3,8 +3,17 @@ import TabBar from '@components/TabBar';
 import * as S from './style';
 
 const MyAccount = () => {
-  const CLIENT_ID = '50e7f2067dc2cf55aa36';
-  const GITHUB_OAUTH_URL = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user%20public_repo`;
+  const CLIENT_ID =
+    process.env.NODE_ENV === 'production'
+      ? process.env.REACT_APP_PROD_CLIENT_ID
+      : process.env.REACT_APP_DEV_CLIENT_ID;
+
+  const REDIRECT_URL =
+    process.env.NODE_ENV === 'production'
+      ? process.env.REACT_APP_PROD_REDIRECT_URL
+      : process.env.REACT_APP_DEV_REDIRECT_URL;
+
+  const GITHUB_OAUTH_URL = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_url=${REDIRECT_URL}&scope=user%20public_repo`;
 
   const loginRequestHandler = () => {
     return window.location.assign(GITHUB_OAUTH_URL);

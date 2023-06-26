a057cbd81fe5a113babe3976e3b9924e0816b21c
saseung <seung0122@naver.com>
2023-06-26 14:54:03 +0900

    feat: 새상품 등록 inputText 뷰 구현 (#122)

--- fe/src/pages/NewProduct/index.tsx
+++ fe/src/pages/NewProduct/index.tsx
@@ -19,6 +19,12 @@ const NewProduct = () => {
         </Link>
       </S.Header>
 
+      <S.LayoutContent>
+        <ImageInput />
+        <S.TextInput placeholder="글제목" />
+        <S.TextInput placeholder="₩ 가격 (선택사항)" />
+        <S.TextArea placeholder="역삼1동에 올릴 게시물 내용을 작성해주세요.(판매금지 물품은 게시가 제한될 수 있어요.)" />
+      </S.LayoutContent>
       <S.TabBar>
         <S.RegionSettingButton>
           <Icon name={ICON_NAME.REGION_SETTING} fill="black" />

60726eec150c8881f274e83540a6acab69e6ed70
YouSooA <kinsk2839@naver.com>
2023-06-27 10:36:11 +0900

    feat: 상세 페이지에 해당 상품에 관련된 데이터를 요청 (#157)

--- fe/src/pages/ProductDetail/index.tsx
+++ fe/src/pages/ProductDetail/index.tsx
@@ -1,78 +1,108 @@
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { ICON_NAME } from '@constants/index';
-
+import { ICON_NAME, REQUEST_URL } from '@constants/index';
 import { getTextWithTimeStamp, formatMoney } from '@utils/index';
+
+import useFetch, { REQUEST_METHOD } from '@hooks/useFetch';
 
 import Icon from '@components/common/Icon';
 import * as S from './style';
 
+interface PostDetailData {
+  id: number;
+  sellerId: number;
+  sellerName: string;
+  title: string;
+  category: string;
+  postedAt: string;
+  content: string;
+  chatCount: number;
+  interestCount: number;
+  viewCount: number;
+  price: number;
+  postState: '광고' | '예약 중' | '판매 중' | '판매 완료';
+  photoUrls: string[];
+  seller: boolean;
+}
+
 const ProductDetail = () => {
+  const { id: postId } = useParams();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { responseState, data: postData } = useFetch<PostDetailData>({
+    url: `${REQUEST_URL.POSTS}/${postId}`,
+    options: {
+      method: REQUEST_METHOD.GET,
+      headers: { Authorization: `Bearer ${localStorage.getItem('Token')}` },
+    },
+  });
 
   return (
     <>
-      <S.Header>
-        <Icon name={ICON_NAME.CHEVRON_LEFT} />
-        <Icon name={ICON_NAME.ELLIPSIS} />
-      </S.Header>
+      {responseState === 'ERROR' && <div>error</div>}
+      {responseState === 'LOADING' && <div>loading</div>}
+      {responseState === 'SUCCESS' && (
+        <>
+          <S.Header>
+            <Icon name={ICON_NAME.CHEVRON_LEFT} />
+            <Icon name={ICON_NAME.ELLIPSIS} />
+          </S.Header>
 
-      <S.Product>
-        <S.ProductImgListLayout>
-          <S.ProductImgList>
-            <S.ProductImg src="https://image.yes24.com/goods/86234361/XL" />
-            <S.ProductImg src="https://image.yes24.com/goods/86234361/XL" />
-          </S.ProductImgList>
-        </S.ProductImgListLayout>
+          <S.Product>
+            <S.ProductImgListLayout>
+              <S.ProductImgList>
+                <S.ProductImg src={postData?.photoUrls[0]} />
+              </S.ProductImgList>
+            </S.ProductImgListLayout>
 
-        <S.ProductInfo>
-          <S.SellerInfo>
-            <span>판매자 정보</span>
-            <span>아켄</span>
-          </S.SellerInfo>
+            <S.ProductInfo>
+              <S.SellerInfo>
+                <span>판매자 정보</span>
+                <span>{postData?.sellerName}</span>
+              </S.SellerInfo>
 
-          <S.PostStateDropDown onClick={() => setIsModalOpen(!isModalOpen)}>
-            <span>판매 중</span>
-            <Icon name={ICON_NAME.CHEVRON_DOWN} />
-            {isModalOpen && (
-              <S.Modal>
-                {['예약 중', '판매 중', '판매 완료'].map((state) => (
-                  <S.Menu key={state} selectedstate={'판매 중'} state={state}>
-                    {state}
-                  </S.Menu>
-                ))}
-              </S.Modal>
-            )}
-          </S.PostStateDropDown>
+              <S.PostStateDropDown onClick={() => setIsModalOpen(!isModalOpen)}>
+                <span>{postData?.postState}</span>
+                <Icon name={ICON_NAME.CHEVRON_DOWN} />
+                {isModalOpen && (
+                  <S.Modal>
+                    {['예약 중', '판매 중', '판매 완료'].map((state) => (
+                      <S.Menu key={state} selectedstate={postData?.postState} state={state}>
+                        {state}
+                      </S.Menu>
+                    ))}
+                  </S.Modal>
+                )}
+              </S.PostStateDropDown>
 
-          <S.Title>빈티지 롤러 스케이트</S.Title>
+              <S.Title>{postData?.title}</S.Title>
 
-          <S.CategoryAndTime>{getTextWithTimeStamp({ text: '가구/인테리어', time: '' })}</S.CategoryAndTime>
+              <S.CategoryAndTime>
+                {getTextWithTimeStamp({ text: postData?.category ?? '', time: postData?.postedAt ?? '' })}
+              </S.CategoryAndTime>
 
-          <S.Content>
-            {
-              '어린시절 추억의 향수를 불러 일으키는 롤러 스케이트입니다. 빈티지 특성상 사용감 있지만 전체적으로 깨끗한 상태입니다.\n\n 촬영용 소품이나, 거실에 장식용으로 추천해 드립니다. 단품 입고 되었습니다. 새제품으로 보존된 제품으로 전용박스까지 보내드립니다.\n\n 사이즈는 235 입니다.'
-            }
-          </S.Content>
+              <S.Content>{postData?.content}</S.Content>
 
-          <S.CountLayout>
-            <S.Count>채팅 0</S.Count>
-            <S.Count>관심 0</S.Count>
-            <S.Count>조회 1</S.Count>
-          </S.CountLayout>
-        </S.ProductInfo>
-      </S.Product>
+              <S.CountLayout>
+                <S.Count>채팅 {postData?.chatCount}</S.Count>
+                <S.Count>관심 {postData?.interestCount}</S.Count>
+                <S.Count>조회 {postData?.viewCount}</S.Count>
+              </S.CountLayout>
+            </S.ProductInfo>
+          </S.Product>
 
-      <S.ToolBar>
-        <S.LikeAndPrice>
-          <Icon name={ICON_NAME.LIKE} />
-          <span>{formatMoney(169000)}</span>
-        </S.LikeAndPrice>
+          <S.ToolBar>
+            <S.LikeAndPrice>
+              <Icon name={ICON_NAME.LIKE} />
+              <span>{formatMoney(postData?.price ?? 0)}</span>
+            </S.LikeAndPrice>
 
-        <S.ChattingButton buttonType="rectangle" buttonState="active">
-          대화 중인 채팅방
-        </S.ChattingButton>
-      </S.ToolBar>
+            <S.ChattingButton buttonType="rectangle" buttonState="active">
+              대화 중인 채팅방
+            </S.ChattingButton>
+          </S.ToolBar>
+        </>
+      )}
     </>
   );
 };

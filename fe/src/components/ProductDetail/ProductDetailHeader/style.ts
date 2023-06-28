0b539b4a76eae1d48fcf07e0d860ec64de15c44f
YouSooA <kinsk2839@naver.com>
2023-06-28 14:40:01 +0900

    feat: Popup 창의 버튼 생성 (#171)

--- fe/src/components/ProductDetail/ProductDetailHeader/style.ts
+++ fe/src/components/ProductDetail/ProductDetailHeader/style.ts
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
 
+const ClosePopupButton = styled.button`
+  width: 100%;
+  max-width: 200px;
+  padding: 8px 0;
+
+  border: 1px solid ${({ theme }) => theme.colors.neutral.border.default};
+  border-radius: 10px;
+
+  color: ${({ theme }) => theme.colors.neutral.text.default};
+`;
+
+const DeletePostButton = styled.button`
+  width: 100%;
+  max-width: 200px;
+  padding: 8px 0;
+
+  border: 1px solid ${({ theme }) => theme.colors.neutral.border.default};
+  border-radius: 10px;
+
+  color: ${({ theme }) => theme.colors.system.warning};
+`;
+
 const Header = styled.header`
   display: flex;
   justify-content: space-between;
@@ -11,4 +33,4 @@ const Header = styled.header`
   padding: 16px;
 `;
 
-export { Header };
+export { ClosePopupButton, DeletePostButton, Header };

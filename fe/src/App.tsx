5de4292d582025b8f934de51f103fdf97553b77f
YouSooA <kinsk2839@naver.com>
2023-06-30 03:21:53 +0900

    feat: App 컴포넌트에 PrivateRoutes 컴포넌트 적용 (#195)

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -18,6 +18,8 @@ import NewProduct from '@pages/NewProduct';
 import ProductDetail from '@pages/ProductDetail';
 import NotFound from '@pages/NotFound';
 
+import PrivateRoutes from '@components/PrivateRoutes';
+
 import GlobalStyle from '@styles/GlobalStyle';
 
 const App = () => {
@@ -28,18 +30,20 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route index path={PATH.HOME} element={<Home />} />
-            <Route path={PATH.REGION_SETTING} element={<RegionSetting />} />
-            <Route path={PATH.CATEGORY} element={<Category />} />
-            <Route path={PATH.CATEGORY_DETAIL} element={<CategoryDetail />} />
-            <Route path={PATH.SALES} element={<Sales />} />
-            <Route path={PATH.INTERESTS} element={<Interests />} />
-            <Route path={PATH.CHATTING} element={<Chatting />} />
-            <Route path={PATH.MY_ACCOUNT} element={<MyAccount />} />
-            <Route path={PATH.LOGIN_LOADING} element={<LoginLoading />} />
-            <Route path={PATH.NEW_PRODUCT} element={<NewProduct />} />
-            <Route path={PATH.PRODUCT_DETAIL}>
-              <Route path={':id'} element={<ProductDetail />} />
+            <Route element={<PrivateRoutes />}>
+              <Route path={PATH.REGION_SETTING} element={<RegionSetting />} />
+              <Route path={PATH.CATEGORY} element={<Category />} />
+              <Route path={PATH.CATEGORY_DETAIL} element={<CategoryDetail />} />
+              <Route path={PATH.SALES} element={<Sales />} />
+              <Route path={PATH.INTERESTS} element={<Interests />} />
+              <Route path={PATH.CHATTING} element={<Chatting />} />
+              <Route path={PATH.NEW_PRODUCT} element={<NewProduct />} />
+              <Route path={PATH.PRODUCT_DETAIL}>
+                <Route path={':id'} element={<ProductDetail />} />
+              </Route>
             </Route>
+            <Route path={PATH.LOGIN_LOADING} element={<LoginLoading />} />
+            <Route path={PATH.MY_ACCOUNT} element={<MyAccount />} />
             <Route path="/*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>

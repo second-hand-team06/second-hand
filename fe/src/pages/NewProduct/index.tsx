6de45d7c3ce25b8608618ea95ca387cfeaf9e66e
saseung <seung0122@naver.com>
2023-06-30 03:08:45 +0900

    refactor: useFetch 수정 (#199)
    - fetch API에서 useFetch 커스텀 훅으로 수정

--- fe/src/pages/NewProduct/index.tsx
+++ fe/src/pages/NewProduct/index.tsx
@@ -1,19 +1,56 @@
 import { useState, ChangeEvent } from 'react';
-import { REQUEST_URL } from '@constants/index';
 import { Link } from 'react-router-dom';
+
+import { REQUEST_URL } from '@constants/index';
 import { ICON_NAME, PATH } from '@constants/index';
+
+import useFetch, { REQUEST_METHOD } from '@hooks/useFetch';
+
 import Icon from '@components/common/Icon';
 import ImageInput from '@components/ImageInput';
-import * as S from './style';
 import TitleInput from '@components/TitleInput';
+import * as S from './style';
 
 const NewProduct = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-  // const [region, setRegion] = useState('');
+  const [region, setRegion] = useState('');
   const [price, setPrice] = useState('');
-  // const [category, setCategory] = useState('');
+  const [category, setCategory] = useState('');
   const [images, setImages] = useState<File[]>([]);
+
+  const { responseState, fetchData } = useFetch({
+    url: REQUEST_URL.POSTS,
+    options: {
+      method: REQUEST_METHOD.POST,
+      headers: { Authorization: `Bearer ${localStorage.getItem('Token')}` },
+    },
+    skip: true,
+  });
+
+  const submitHandler = async () => {
+    const formData = new FormData();
+
+    formData.append('title', title);
+    formData.append('regionId', `${1}`);
+    formData.append('categoryId', `${1}`);
+    formData.append('price', price);
+    formData.append('badgeId', `${1}`);
+    formData.append('content', content);
+
+    if (images.length > 0) {
+      for (let i = 0; i < images.length; i++) {
+        formData.append('photos', images[i]);
+      }
+    }
+
+    await fetchData(formData);
+
+    if (responseState === 'ERROR') {
+      alert('상품 상태 수정에 실패했습니다.');
+      return;
+    }
+  };
 
   const titleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -42,42 +79,6 @@ const NewProduct = () => {
 
   const deleteImageHandler = (image: File) => {
     setImages((previousImages) => previousImages.filter((img) => img !== image));
-  };
-
-  const submitHandler = () => {
-    const formData = new FormData();
-
-    formData.append('title', title);
-    formData.append('regionId', `${1}`);
-    formData.append('categoryId', `${1}`);
-    formData.append('price', price);
-    formData.append('badgeId', `${1}`);
-    formData.append('content', content);
-
-    if (images.length > 0) {
-      for (let i = 0; i < images.length; i++) {
-        formData.append('photos', images[i]);
-      }
-    }
-
-    fetch(REQUEST_URL.POSTS, {
-      method: 'post',
-      headers: {
-        Authorization:
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsb2dpbl9tZW1iZXIiLCJ1c2VyUHJvZmlsZSI6eyJpZCI6MiwiZ2l0aHViSWQiOjcxMTYyMzkwLCJsb2dpbklkIjoia2ltIHNlb25nZ3l1IiwicHJvZmlsZVVybCI6Imh0dHBzOi8vYXZhdGFycy5naXRodWJ1c2VyY29udGVudC5jb20vdS83MTE2MjM5MD92PTQifSwiZXhwIjoxNjg5NTgzNzAyfQ.tSyzbXZBhNl2aBj-s69WUNptiybtXF5TtROCGHoPc78',
-      },
-      body: formData,
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          alert('저장 완료');
-        } else if (res.status === 403) {
-          return res.json();
-        }
-      })
-      .then(() => {
-        console.log('에러 메시지 ->');
-      });
   };
 
   return (

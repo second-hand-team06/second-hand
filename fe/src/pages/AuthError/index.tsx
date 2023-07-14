9cab3b0b29ef5834e2fe2a5ff0dd5fe591ec431c
YouSooA <kinsk2839@naver.com>
2023-07-15 00:49:41 +0900

    feat: AuthError 페이지 생성 (#214)

---
+++ fe/src/pages/AuthError/index.tsx
@@ -0,0 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
+import { useUserContext } from '@context/userContext';
+import { PATH } from '@constants/path';
+
+import Popup from '@components/common/Popup';
+import * as S from './style';
+
+interface AuthErrorProps {
+  resetErrorBoundary: () => void;
+}
+
+const AuthError = ({ resetErrorBoundary }: AuthErrorProps) => {
+  const { logout } = useUserContext();
+  const navigate = useNavigate();
+
+  const goToLoginPageHandler = () => {
+    logout();
+    resetErrorBoundary();
+    navigate(PATH.MY_ACCOUNT);
+  };
+
+  return (
+    <Popup text="다시 로그인해주세요.">
+      <S.LoginPageButton onClick={goToLoginPageHandler}>로그인 페이지로 이동</S.LoginPageButton>
+    </Popup>
+  );
+};
+
+export default AuthError;

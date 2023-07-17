7c91b7446e4d6f0dd0a6cd20dd6de84248916764
YouSooA <kinsk2839@naver.com>
2023-07-17 16:45:00 +0900

    feat: UnknownError 페이지 생성 (#214)

---
+++ fe/src/pages/ErrorPage/UnknownError/index.tsx
@@ -0,0 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
+import { PATH } from '@constants/path';
+
+import logoImg from '@assets/second-hand-logo.png';
+import * as S from './style';
+
+interface ServerErrorProps {
+  resetErrorBoundary: () => void;
+}
+
+const UnknownError = ({ resetErrorBoundary }: ServerErrorProps) => {
+  const navigate = useNavigate();
+
+  const goToHomePageHandler = () => {
+    resetErrorBoundary();
+    navigate(PATH.HOME);
+  };
+
+  return (
+    <S.UnknownError>
+      <S.LogoImg src={logoImg} alt="second-hand-log" />
+      <S.Message>알 수 없는 오류가 발생했습니다.</S.Message>
+      <S.Message>관리자에게 문의해주세요.</S.Message>
+      <S.HomeButton onClick={goToHomePageHandler}>Home 페이지로 이동</S.HomeButton>
+    </S.UnknownError>
+  );
+};
+
+export default UnknownError;

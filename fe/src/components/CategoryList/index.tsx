8bc924716621c0c7f3a517a7baf389cd60be30e3
saseung <seung0122@naver.com>
2023-07-19 06:25:08 +0900

    feat: 카테고리 리스트 컴포넌트 구현
    - 카테고리 리스트 fetch 해온 후 목록을 보여줌
    - 컴포넌트 및 스타일 적용

---
+++ fe/src/components/CategoryList/index.tsx
@@ -0,0 +1,49 @@
+import { ICON_NAME, REQUEST_URL } from '@constants/index';
+
+import useFetch, { REQUEST_METHOD } from '@hooks/useFetch';
+
+import Icon from '@components/common/Icon';
+import * as S from './style';
+
+interface Category {
+  id: number;
+  name: string;
+}
+
+interface CategoriesData {
+  categories: Category[];
+}
+
+const CategoryList = () => {
+  const { responseState, data } = useFetch<CategoriesData>({
+    url: REQUEST_URL.CATEGORY,
+    options: {
+      method: REQUEST_METHOD.GET,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('Token')}`,
+      },
+    },
+  });
+
+  return (
+    <S.CategoryList>
+      <S.Header>
+        <S.BackButton>
+          <Icon name={ICON_NAME.CHEVRON_LEFT} />
+          <span>닫기</span>
+        </S.BackButton>
+        <S.HeaderTitle>카테고리</S.HeaderTitle>
+        <S.EmptyTag />
+      </S.Header>
+      {responseState === 'SUCCESS' && (
+        <S.CategoryListLayout>
+          {data?.categories.map(({ id, name }) => (
+            <S.CategoryItem key={id}>{name}</S.CategoryItem>
+          ))}
+        </S.CategoryListLayout>
+      )}
+    </S.CategoryList>
+  );
+};
+
+export default CategoryList;
---
+++ fe/src/components/CategoryList/style.ts
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+const CategoryList = styled.div`
+  position: absolute;
+  top: 0;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: ${({ theme }) => theme.colors.neutral.background.default};
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 44px;
+  padding: 16px;
+
+  background-color: ${({ theme }) => theme.colors.neutral.background.blur};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.neutral.border.default};
+`;
+
+const HeaderTitle = styled.span`
+  font-size: ${({ theme }) => theme.fonts.body.bold.fontSize};
+  font-weight: ${({ theme }) => theme.fonts.body.bold.fontWeight};
+  line-height: ${({ theme }) => theme.fonts.body.bold.lineHeight};
+  color: ${({ theme }) => theme.colors.neutral.text.strong};
+`;
+
+const BackButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  width: 100px;
+
+  font-size: ${({ theme }) => theme.fonts.body.regular.fontSize};
+  font-weight: ${({ theme }) => theme.fonts.body.regular.fontWeight};
+  line-height: ${({ theme }) => theme.fonts.body.regular.lineHeight};
+  color: ${({ theme }) => theme.colors.neutral.text.strong};
+`;
+
+const EmptyTag = styled.div`
+  width: 100px;
+`;
+
+const CategoryListLayout = styled.ul`
+  margin: 0 16px;
+`;
+
+const CategoryItem = styled.li`
+  padding: 16px 0px;
+
+  font-size: ${({ theme }) => theme.fonts.subhead.fontSize};
+  font-weight: ${({ theme }) => theme.fonts.subhead.fontWeight};
+  line-height: ${({ theme }) => theme.fonts.subhead.lineHeight};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.neutral.border.default};
+`;
+
+export { CategoryList, Header, HeaderTitle, BackButton, EmptyTag, CategoryListLayout, CategoryItem };

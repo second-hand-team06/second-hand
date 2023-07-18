bd64d54f1443b4c3975f7e29d259ffd005e37956
saseung <seung0122@naver.com>
2023-07-19 06:27:59 +0900

    feat: [>] 버튼 클릭 시 카테고리 리스트 포탈처리

--- fe/src/components/TitleInput/index.tsx
+++ fe/src/components/TitleInput/index.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
+
 import { ICON_NAME, REQUEST_URL } from '@constants/index';
+
 import useFetch, { REQUEST_METHOD } from '@hooks/useFetch';
+
 import Icon from '@components/common/Icon';
+import CategoryList from '@components/CategoryList';
+import ModalPortal from '@components/ModalPortal';
 import * as S from './style';
+
 
 interface TitleInputProps {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  // onClick: (e: React.MouseEvent) => void;
 }
 
 interface CategoriesData {
@@ -14,6 +21,12 @@ interface CategoriesData {
 
 const TitleInput = ({ onChange }: TitleInputProps) => {
   const [selectCategory, setSelectCategory] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+
+  const modalHandler = () => {
+    setShowModal((prev) => !prev);
+  };
+
   const token = localStorage.getItem('Token');
   const options: RequestInit = {
     method: REQUEST_METHOD.GET.toString(),
@@ -30,24 +43,33 @@ const TitleInput = ({ onChange }: TitleInputProps) => {
   };
 
   return (
-    <S.TitleInputLayout>
-      <S.TitleInput placeholder="글 제목" onChange={onChange}></S.TitleInput>
+    <>
+      {showModal && (
+        <ModalPortal>
+          <CategoryList />
+        </ModalPortal>
+      )}
+      <S.TitleInputLayout>
+        <S.TitleInput placeholder="글 제목" onChange={onChange}></S.TitleInput>
 
-      <S.CategoryLayout>
-        <S.CategoryList>
-          {data?.categories.map(({ id, name }, idx) => (
-            <S.CategoryItem
-              key={id}
-              className={selectCategory === idx ? 'active' : ''}
-              onClick={(e) => selectCategoryHandler(e, idx)}
-            >
-              {name}
-            </S.CategoryItem>
-          ))}
-        </S.CategoryList>
-        <Icon name={ICON_NAME.CHEVRON_RIGHT} />
-      </S.CategoryLayout>
-    </S.TitleInputLayout>
+        <S.CategoryLayout>
+          <S.CategoryList>
+            {data?.categories.map(({ id, name }, idx) => (
+              <S.CategoryItem
+                key={id}
+                className={selectCategory === idx ? 'active' : ''}
+                onClick={(e) => selectCategoryHandler(e, idx)}
+              >
+                {name}
+              </S.CategoryItem>
+            ))}
+          </S.CategoryList>
+          <S.CategoryListButton onClick={modalHandler}>
+            <Icon name={ICON_NAME.CHEVRON_RIGHT} />
+          </S.CategoryListButton>
+        </S.CategoryLayout>
+      </S.TitleInputLayout>
+    </>
   );
 };
 

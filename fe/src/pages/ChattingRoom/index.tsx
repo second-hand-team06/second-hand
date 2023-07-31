164255a6fb7a8a154cfd570cdf7c51ffa30ab919
YouSooA <kinsk2839@naver.com>
2023-07-31 23:16:49 +0900

    feat: 해당 채팅방의 데이터 fetch 요청 (#231)
    - chatting-room/:id 페이지 진입 시 fetch 요청

--- fe/src/pages/ChattingRoom/index.tsx
+++ fe/src/pages/ChattingRoom/index.tsx
@@ -1,17 +1,57 @@
-import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { ICON_NAME } from '@constants/index';
+import { useUserContext } from '@context/userContext';
+import useFetch, { REQUEST_METHOD } from '@hooks/useFetch';
+
+import { REQUEST_URL, ICON_NAME } from '@constants/index';
 
 import Icon from '@components/common/Icon';
 import * as S from './style';
 
+type Participant = {
+  id: number;
+  name: string;
+  url: string;
+};
+
+type Message = {
+  roomId: string;
+  senderId: number;
+  content: string;
+  createdAt: Date;
+};
+
+interface ChatRoomData {
+  participants: Participant[];
+  product: {
+    id: number;
+    title: string;
+    price: number;
+
+    photoUrl: string;
+  };
+  unreadCount: number;
+  lastMessage: Message;
+  createdAt: Date;
+}
+
 const ChattingRoom = () => {
+  const { id: roomId } = useParams();
+  const { user } = useUserContext();
+
+  const { data } = useFetch<ChatRoomData>({
+    url: `${REQUEST_URL.CHAT_ROOMS}/${roomId}`,
+    options: {
+      method: REQUEST_METHOD.GET,
+      headers: { Authorization: `Bearer ${localStorage.getItem('Token')}` },
+    },
+  });
+
   return (
     <>
       <S.Header>
         <Icon name={ICON_NAME.CHEVRON_LEFT} />
-        <S.SenderName>{}</S.SenderName>
+        <S.SenderName>{data?.participants.find(({ id }) => id !== user?.id)?.name}</S.SenderName>
         <Icon name={ICON_NAME.ELLIPSIS} />
       </S.Header>
     </>

7e7a07457202867bd60f0554d2b2813452720d91
saseungg <yeon14766@gmail.com>
2023-10-03 08:17:01 +0900

    feat: 아이콘 props 사이즈 추가
    - 탭바 아이콘 사이즈 수정이 필요하여 아이콘 컴포넌트에 사이즈를 props로 전달할 수 있도록 수정

--- fe/src/components/TabBar/index.tsx
+++ fe/src/components/TabBar/index.tsx
@@ -14,31 +14,31 @@ const TabBar = ({ activeTab }: TabBarProps) => {
     <S.TabBar>
       <Link to={PATH.HOME}>
         <S.Tab name="home" activetab={activeTab}>
-          <Icon name={ICON_NAME.HOME} />
+          <Icon name={ICON_NAME.HOME} size={20} />
           <span>홈</span>
         </S.Tab>
       </Link>
       <Link to={PATH.SALES}>
         <S.Tab name="sales" activetab={activeTab}>
-          <Icon name={ICON_NAME.NEWSPAPER} />
+          <Icon name={ICON_NAME.NEWSPAPER} size={20}/>
           <span>판매내역</span>
         </S.Tab>
       </Link>
       <Link to={PATH.INTERESTS}>
         <S.Tab name="interests" activetab={activeTab}>
-          <Icon name={ICON_NAME.LIKE} />
+          <Icon name={ICON_NAME.LIKE} size={20}/>
           <span>관심목록</span>
         </S.Tab>
       </Link>
       <Link to={PATH.CHATTING}>
         <S.Tab name="chatting" activetab={activeTab}>
-          <Icon name={ICON_NAME.MESSAGE} />
+          <Icon name={ICON_NAME.MESSAGE} size={20}/>
           <span>채팅</span>
         </S.Tab>
       </Link>
       <Link to={PATH.MY_ACCOUNT}>
         <S.Tab name="myAccount" activetab={activeTab}>
-          <Icon name={ICON_NAME.PERSON} />
+          <Icon name={ICON_NAME.PERSON} size={20}/>
           <span>내 계정</span>
         </S.Tab>
       </Link>

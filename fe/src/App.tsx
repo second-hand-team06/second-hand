bf8928fb6d950459be4e0ae60a75fc418078295e
saseungg <yeon14766@gmail.com>
2023-10-08 05:48:03 +0900

    refactor: 라우터 부모 요소로 레이아웃 컴포넌트 적용 (#7)

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -20,6 +20,7 @@ import MyAccount from '@pages/MyAccount';
 import LoginLoading from '@pages/LoginLoading';
 import NewProduct from '@pages/NewProduct';
 import ProductDetail from '@pages/ProductDetail';
+import Layout from '@components/common/Layout';
 
 import ErrorBoundary from '@components/ErrorBoundary/ErrorBoundary';
 import PrivateRoutes from '@components/PrivateRoutes';
@@ -37,24 +38,26 @@ const App = () => {
             ServerErrorFallback={ServerError}
             UnknownErrorFallback={UnknownError}
           >
-            <Routes>
-              <Route index path={PATH.HOME} element={<Home />} />
-              <Route element={<PrivateRoutes />}>
-                <Route path={PATH.REGION_SETTING} element={<RegionSetting />} />
-                <Route path={PATH.CATEGORY} element={<Category />} />
-                <Route path={PATH.CATEGORY_DETAIL} element={<CategoryDetail />} />
-                <Route path={PATH.SALES} element={<Sales />} />
-                <Route path={PATH.INTERESTS} element={<Interests />} />
-                <Route path={PATH.CHATTING} element={<Chatting />} />
-                <Route path={PATH.NEW_PRODUCT} element={<NewProduct />} />
-                <Route path={PATH.PRODUCT_DETAIL}>
-                  <Route path={':id'} element={<ProductDetail />} />
+            <Layout>
+              <Routes>
+                <Route index path={PATH.HOME} element={<Home />} />
+                <Route element={<PrivateRoutes />}>
+                  <Route path={PATH.REGION_SETTING} element={<RegionSetting />} />
+                  <Route path={PATH.CATEGORY} element={<Category />} />
+                  <Route path={PATH.CATEGORY_DETAIL} element={<CategoryDetail />} />
+                  <Route path={PATH.SALES} element={<Sales />} />
+                  <Route path={PATH.INTERESTS} element={<Interests />} />
+                  <Route path={PATH.CHATTING} element={<Chatting />} />
+                  <Route path={PATH.NEW_PRODUCT} element={<NewProduct />} />
+                  <Route path={PATH.PRODUCT_DETAIL}>
+                    <Route path={':id'} element={<ProductDetail />} />
+                  </Route>
                 </Route>
-              </Route>
-              <Route path={PATH.LOGIN_LOADING} element={<LoginLoading />} />
-              <Route path={PATH.MY_ACCOUNT} element={<MyAccount />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
+                <Route path={PATH.LOGIN_LOADING} element={<LoginLoading />} />
+                <Route path={PATH.MY_ACCOUNT} element={<MyAccount />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </Layout>
           </ErrorBoundary>
         </BrowserRouter>
       </ThemeProvider>
